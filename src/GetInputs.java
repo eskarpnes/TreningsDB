ce31e629a7991f8ab61c69c7472befebdf4ef184
tsm121 <tsm121@gmail.com>
2017-03-13 17:05:15 +0100

    Result done. PS! Tollef er bakfull

--- src/GetInputs.java
+++ src/GetInputs.java
@@ -47,10 +47,37 @@ public class GetInputs {
 
     }
 
+    public String getStringInputFreetext(String text ){
+
+        this.input = new Scanner(System.in);
+        System.out.println(text);
+        System.out.print(">>> ");
+        String value = "";
+        if (this.input.hasNextLine()) {
+            value = this.input.nextLine();
+        }
+
+        return value;
+
+    }
+
     public Integer getIntInput(String type ){
 
         this.input = new Scanner(System.in);
         System.out.println("Your " + type + ": ");
+        System.out.print(">>> ");
+        int value = -1;
+        if (this.input.hasNextLine()) {
+            value = this.input.nextInt();
+        }
+
+        return value;
+    }
+
+    public Integer getIntInputFreetext(String text ){
+
+        this.input = new Scanner(System.in);
+        System.out.println(text);
         System.out.print(">>> ");
         int value = -1;
         if (this.input.hasNextLine()) {
--- src/Result.java
+++ src/Result.java
@@ -1,31 +1,103 @@
+import java.util.Scanner;
 
-public class Result extends GetInputs {
+public class Result {
 
-    public String goal;
+    public GetInputs input = new GetInputs();
+
+    public String exType = "";
+    public String exLocation = "";
+    public String exName = "";
     public String description;
+    public String goal;
+    public int workload;
+    public int reps;
+    public int sets;
+
 
     public static void main(String[] args) {
 
-        new Result();
+        System.out.println(new Result());
 
     }
 
     public Result(){
 
+        setCategory();
+        setExName();
         setDescription();
         setGoal();
+        setWorkload();
+        setReps();
+        setSets();
+
+    }
+
+    private void setCategory(){
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Where did you do your workout? 1 = inside, 2 = outside");
+        System.out.println(">>> ");
+        int locInt = in.nextInt();
+
+        System.out.println("What kind of exercise you do? 1 = cardio, 2 = bodybuilding" );
+        System.out.println(">>> ");
+        int typeInt = in.nextInt();
+
+        if (locInt == 1) {this.exLocation = "inside";}
+        else {this.exLocation = "outside";}
+
+        if (typeInt == 1) {this.exType = "cardio";}
+        else {this.exType = "bodybuilding";}
+
+    }
+
+    private void setExName(){
+
+        this.exName = input.getStringInputFreetext("Name your exercise:");
 
     }
 
     private void setDescription() {
 
-        this.description = getStringInput("description");
+        this.description = input.getStringInput("description");
 
     }
 
     private void setGoal(){
 
-        this.goal = getStringInput("goal");
+        this.goal = input.getStringInput("goal");
+
+    }
+
+    private void setWorkload(){
+
+        this.workload = input.getIntInput("workload", (this.exType.equals("bodybuilding") ? " in kg" : " in km"));
+
+    }
+
+    private void setReps(){
+
+        this.reps = input.getIntInputFreetext("How many reps did you do?");
+
+    }
+
+    private void setSets(){
+
+        this.sets = input.getIntInputFreetext("How many sets did you do?");
+
+    }
+
+    @Override
+    public String toString() {
+
+        return ("Description: " + this.description
+                + '\n' + "Exercise name: " + this.exName
+                + '\n' + "Exercise type: " + this.exType
+                + '\n' + "Exercise location: " + this.exLocation
+                + '\n' + "Goal: " + this.goal
+                + '\n' + "Workload: " + this.workload
+                + '\n' + "Number of reps: " + Integer.toString(this.reps)
+                + '\n' + "Number of sets: " + Integer.toString(this.sets));
 
     }
 }

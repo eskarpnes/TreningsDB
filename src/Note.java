d35e478b939a493d2019b5df00a3e3bec0dfddf8
tsm121 <tsm121@gmail.com>
2017-03-13 15:16:02 +0100

    Note done

--- src/Note.java
+++ src/Note.java
@@ -2,4 +2,21 @@
  * Created by Erlend on 13.03.2017.
  */
 public class Note {
+
+    private String notePurpose, noteTips;
+
+    public Note(String purpose, String tips){
+
+        notePurpose = purpose;
+        noteTips = tips;
+
+    }
+
+    public String getPurpose() {
+        return notePurpose;
+    }
+
+    public String getTips() {
+        return noteTips;
+    }
 }
